ac9d50c5dc1792e4789d2a2ac86a242af072d832
David Hudson <davidhudsont@gmail.com>
2021-09-06 16:27:20 -0700

    Remove unused includes from commands

--- src/ConsoleCommands.cpp
+++ src/ConsoleCommands.cpp
@@ -1,13 +1,9 @@
 #include "ConsoleCommands.h"
-#include <stdio.h>
-#include <string.h>
 #include "esp_log.h"
 #include "esp_console.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "argtable3/argtable3.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
 #include "DeviceCommands.h"
 
 static void register_version(void);
